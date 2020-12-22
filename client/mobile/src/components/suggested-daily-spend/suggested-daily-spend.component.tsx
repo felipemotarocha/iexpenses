030740f82d3f99913c93a01f86fda30c347878df
Felipe Rocha <felipemota.rocha@gmail.com>
2020-12-21 23:40:55 -0300

    feat [mobile]: converted to english, added today date

--- client/mobile/src/components/suggested-daily-spend/suggested-daily-spend.component.tsx
+++ client/mobile/src/components/suggested-daily-spend/suggested-daily-spend.component.tsx
@@ -3,16 +3,20 @@ import * as React from 'react';
 import Card from '../card/card.component';
 import {Headline, Spend} from './suggested-daily-spend.styles';
 import {Button} from 'react-native-paper';
+import {Text} from 'react-native';
+import {format} from 'date-fns';
 
 export interface SuggestedDailySpendProps {}
 
 const SuggestedDailySpend: React.FC<SuggestedDailySpendProps> = () => {
+  const {current: today} = React.useRef(format(new Date(), 'EEEE, LLLL do'));
   return (
     <Card>
-      <Headline>Quanto você pode gastar hoje:</Headline>
+      <Text>{today}</Text>
+      <Headline>How much you can spend today:</Headline>
       <Spend>R$50,00</Spend>
       <Button mode="contained" dark style={{width: '60%'}}>
-        Adicionar despesa
+        Add Expense
       </Button>
     </Card>
   );
