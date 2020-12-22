619e308de0e12f05f7ac8c6f268c7eef3707cdb1
Felipe Rocha <felipemota.rocha@gmail.com>
2020-12-21 22:15:33 -0300

    feat [mobile]: initial commit

---
+++ client/mobile/src/App/App.tsx
@@ -0,0 +1,10 @@
+import * as React from 'react';
+import {Text} from 'react-native';
+
+export interface AppProps {}
+
+const App: React.FC<AppProps> = () => {
+  return <Text>hello world!</Text>;
+};
+
+export default App;
